94879e59ec50f24da3c488d4fde15cf593387301
Ayat Tulloh Rahulloh Khomeini <5026211156@mhs.its.ac.id>
2024-04-19 15:42:15 +0700

    Refactor count_repititions.py: Add gyroscope data visualization and configure LowPassFilter

--- src/features/count_repititions.py
+++ src/features/count_repititions.py
@@ -46,20 +46,32 @@ plot_df[plot_df["set"] == plot_df["set"].unique()[0]]["acc_y"].plot(figsize=(20,
 plot_df[plot_df["set"] == plot_df["set"].unique()[0]]["acc_z"].plot(figsize=(20, 5), title="Bench Press", legend=True)
 plot_df[plot_df["set"] == plot_df["set"].unique()[0]]["acc_r"].plot(figsize=(20, 5), title="Bench Press", legend=True)
 
+plot_df[plot_df["set"] == plot_df["set"].unique()[0]]["gyr_x"].plot(figsize=(20, 5), title="Bench Press", legend=True)
+plot_df[plot_df["set"] == plot_df["set"].unique()[0]]["gyr_y"].plot(figsize=(20, 5), title="Bench Press", legend=True)
+plot_df[plot_df["set"] == plot_df["set"].unique()[0]]["gyr_z"].plot(figsize=(20, 5), title="Bench Press", legend=True)
+plot_df[plot_df["set"] == plot_df["set"].unique()[0]]["gyr_r"].plot(figsize=(20, 5), title="Bench Press", legend=True)
+
 # --------------------------------------------------------------
 # Configure LowPassFilter
 # --------------------------------------------------------------
-
-
+fs = 1000/200
+lowpass = LowPassFilter()
 # --------------------------------------------------------------
 # Apply and tweak LowPassFilter
 # --------------------------------------------------------------
-
+bench_set = bench_df[bench_df["set"] == bench_df["set"].unique()[0]]
+squat_set = squat_df[squat_df["set"] == squat_df["set"].unique()[0]]
+dead_set = dead_df[dead_df["set"] == dead_df["set"].unique()[0]]
+ohp_set = ohp_df[ohp_df["set"] == ohp_df["set"].unique()[0]]
+row_set = row_df[row_df["set"] == row_df["set"].unique()[0]]
 
 # --------------------------------------------------------------
 # Create function to count repetitions
 # --------------------------------------------------------------
-
+column = "acc_r"
+lowpass.low_pass_filter(
+    bench_set, col=column, sampling_frequency=fs, cutoff_frequency=0.4, order=5
+)[column + "_lowpass"].plot(figsize=(20, 5), legend=True)
 
 # --------------------------------------------------------------
 # Create benchmark dataframe
